c987e52dbd6f179891814e9a5885eba5381bbd17
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-03-11 16:49:41 +0330

    change [profile

--- pages/panel/profile/index.vue
+++ pages/panel/profile/index.vue
@@ -1,83 +1,100 @@
 <template>
-    <div>
-        <NuxtLayout name="panel">
-            <v-col cols="12" sm="12">
+  <div>
+    <NuxtLayout name="panel">
+      <v-col cols="12" sm="12">
+        <v-card class="mx-auto" max-width="1000">
+          <v-img
+            class="mx-auto"
+            height="300"
+            max-width="500"
+            src="https://bad.src/not/valid"
+          >
+            <template v-slot:error>
+              <v-img
+                class="mx-auto"
+                height="300"
+                max-width="500"
+                src="https://picsum.photos/500/300?image=232"
+              ></v-img>
+            </template>
+          </v-img>
+          <v-col
+            cols="11"
+            class="align-center d-flex justify-center justify-space-between mt-10"
+          >
+            <div class="d-flex align-center">
+              <v-card-subtitle>
+                <h3>name :</h3>
+              </v-card-subtitle>
 
-                <v-card class="mx-auto" max-width="1000">
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover>
+              <v-card-subtitle>
+                {{ authUser?.name }}
+              </v-card-subtitle>
+            </div>
+            <div class="d-flex">
+              <v-card-subtitle>
+                <h3>email :</h3>
+              </v-card-subtitle>
 
-                    </v-img>
-                        <v-col cols="11" class=" align-center d-flex justify-center justify-space-between mt-10">
-                            <div class="d-flex align-center">
-                                <v-card-subtitle>
-                                    <h3>name :</h3>
-                                </v-card-subtitle>
+              <v-card-subtitle>
+                {{ authUser?.email }}
+              </v-card-subtitle>
+            </div>
+            <div class="d-flex">
+              <v-card-subtitle>
+                <h3>phone :</h3>
+              </v-card-subtitle>
 
-                                <v-card-subtitle>
-                                    {{  authUser?.name  }}
-                                </v-card-subtitle>
-                            </div>
-                            <div class="d-flex ">
-                                <v-card-subtitle>
-                                   <h3> email :</h3>
-                                </v-card-subtitle>
+              <v-card-subtitle>
+                {{ authUser?.phone }}
+              </v-card-subtitle>
+            </div>
+            <div class="d-flex">
+              <v-card-subtitle>
+                <h3>status :</h3>
+              </v-card-subtitle>
 
-                                <v-card-subtitle>
-                                   {{  authUser?.email }}
-                                </v-card-subtitle>
-                            </div>
-                            <div class="d-flex ">
-                                <v-card-subtitle>
-                                  <h3>  phone :</h3>
-                                </v-card-subtitle>
+              <v-card-subtitle>
+                {{ authUser?.status }}
+              </v-card-subtitle>
+            </div>
+          </v-col>
+          <v-card-actions>
+            <v-btn color="black" variant="text">
+              click me !
+            </v-btn>
 
-                                <v-card-subtitle>
-                                   {{  authUser?.phone  }}
-                                </v-card-subtitle>
-                            </div>
-                            <div class="d-flex ">
-                                <v-card-subtitle>
-                                    <h3>status :</h3>
-                                </v-card-subtitle>
+            <v-spacer></v-spacer>
 
-                                <v-card-subtitle>
-                                  {{ authUser?.status  }}
-                                </v-card-subtitle>
-                            </div>
-                        </v-col>
-                    <v-card-actions>
-                        <v-btn color="black" variant="text">
-                            برای دیدن توضیعات دکمه روبهرور بزنید 
-                        </v-btn>
+            <v-btn
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show = !show"
+            ></v-btn>
+          </v-card-actions>
 
-                        <v-spacer></v-spacer>
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
 
-                        <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-                                I'm a thing. But, like most politicians, he promised more than he could deliver. You
-                                won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then
-                                we'll go with that data file! Hey, you add a one and two zeros to that or we walk!
-                                You're going to do his laundry? I've got to find a way to escape.
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
-                </v-card>
-            </v-col>
-        </NuxtLayout>
-    </div>
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </NuxtLayout>
+  </div>
 </template>
 
 <script setup>
-const show = ref(false)
+const show = ref(false);
 
-const { authUser } = useAuth() ; 
-
-
+const { authUser } = useAuth();
 </script>
 
